d1a20d95620a56cfb6aff9b55735f941057e5a5e
Lazare Mirziashvili <lazzexa@gmail.com>
2025-05-01 01:21:36 +0400

    navbar needs work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent,title:'Home - Lazare Mirziashvili'},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'projects',component:ProjectsComponent,title:'Projects - Lazare Mirziashvili'},
+  {path:'services',component:ServicesComponent,title:'Services - Lazare Mirziashvili'},
+  {path:'contact',component:ContactComponent,title:'Contact - Lazare Mirziashvili'},
+  {path:'**',component:PageNotFoundComponent,title:'Page Not Found - Lazare Mirziashvili'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ServicesComponent } from './services/services.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ContactComponent,
+    ProjectsComponent,
+    ServicesComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
