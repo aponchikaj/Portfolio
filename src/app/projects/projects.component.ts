4c8051839ae7595b974704147d953c6dd809512e
aponchikaj <lazzexa@gmail.com>
2025-05-01 14:28:57 +0400

    projects morcha

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-
+  //Projects Array to Display
+  AssetsString:String = '../../assets/'
+  Projects = [
+    {
+      article:'Poster Shop From Tbilisi.',
+      name:'Illusion',
+      image:`${this.AssetsString}projects/illusion.PNG`,
+      url:'https://iluzia.vercel.app/'
+    },
+    {
+      article:'Blogs website',
+      name:'Lazares Blogs',
+      image:`${this.AssetsString}projects/blogs.PNG`,
+      url:'https://lazaresblog.vercel.app/home'
+    },
+    {
+      article:'Osiris Shoes Website (FAKE)',
+      name:'Osiris',
+      image:`${this.AssetsString}projects/Osiris.PNG`,
+      url:'https://osiris-lazare.vercel.app/'
+    },
+    {
+      article:'Information about Die antwoord.',
+      name:"Whos Die Antwoord",
+      image:`${this.AssetsString}projects/antwoord.PNG`,
+      url:'https://whos-dieantwoord.vercel.app/'
+    },
+    {
+      article:'Todo App',
+      name:'Todo',
+      image:`${this.AssetsString}projects/todo.PNG`,
+      url:'https://todo-by-lazare.vercel.app/'
+    },
+    {
+      article:'Bot named Lisa',
+      name:'Lisa',
+      image:`${this.AssetsString}projects/lisa.PNG`,
+      url:'https://lisa-bot.vercel.app/'
+    },
+  ]
 }
